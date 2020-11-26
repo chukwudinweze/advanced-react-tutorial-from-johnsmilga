62be093e435b7ec487b97987756708f593834490
chukwudinweze <chukwudinwez@gmail.com>
2020-11-26 05:09:58 +0100

    used what I learnt in useState hook to build a counter app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Setup from './tutorial/1-useState/setUp/4-use-State-object.js'
+import Setup from "./tutorial/1-useState/setUp/5-useState-counter.js";
 function App() {
   return (
     <div className="container">
-      <Setup/>
+      <Setup />
     </div>
   );
 }
