4f6abab6db40ee00f71ca232c5364c730f4a8a20
chukwudinweze <chukwudinwez@gmail.com>
2020-12-01 22:32:23 +0100

    learnt about short-circuit evaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,20 @@ import React, { useState } from "react";
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [Error, setError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <div>
+      <h1> {text || "John Doe"}</h1>
+      <button className="btn" onClick={() => setError(!Error)}>
+        toggle Error
+      </button>
+      {Error && <h1>John Dore</h1>}
+    </div>
+  );
 };
 
 export default ShortCircuit;
