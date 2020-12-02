8bb0c450976e5fc4577679b13b72311e74cbafd1
chukwudinweze <chukwudinwez@gmail.com>
2020-12-02 23:46:52 +0100

    learnt to use short circuit evaluation to toggle between elements and components

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,38 @@
 import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      <button
+        className="btn"
+        style={{ marginTop: "2rem" }}
+        onClick={() => setShow(!show)}
+      >
+        toggle component
+      </button>
+      {show && <Item />}
+    </div>
+  );
+};
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  });
+  return (
+    <div>
+      <h1>Window</h1>
+      <h3>size:{size}</h3>
+    </div>
+  );
 };
 
 export default ShowHide;
